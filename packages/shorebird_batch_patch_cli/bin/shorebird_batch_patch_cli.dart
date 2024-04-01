06112da278b9233253b4370fc263925c6570749a
Josiah Saunders <contact@josiahsaunders.com>
2024-04-01 10:12:32 -0700

    init package

---
+++ packages/shorebird_batch_patch_cli/bin/shorebird_batch_patch_cli.dart
@@ -0,0 +1,5 @@
+import 'package:shorebird_batch_patch_cli/shorebird_batch_patch_cli.dart' as shorebird_batch_patch_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shorebird_batch_patch_cli.calculate()}!');
+}
---
+++ packages/shorebird_batch_patch_cli/test/shorebird_batch_patch_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:shorebird_batch_patch_cli/shorebird_batch_patch_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
